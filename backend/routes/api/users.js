88e07b83e96279984ccd998b0afcf71125b63897
Toty <tobiaslevy1409@gmail.com>
2023-05-29 19:34:20 -0300

    Express-level validation for new users

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,9 +1,34 @@
 //For routes begginig with api/users
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const {check} = require("express-validator");
+const {handleValidationErrors} = require("../../utils/validation");
 const router = require("express").Router();
 
-router.post("/", async (req, res) => {
+const validateSignup = [
+    check('username')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .isLength({min: 4})
+        .withMessage("Please provide a username with at least 4 characters."),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage("Username should not be an email."),
+    check('email')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .isEmail()
+        .withMessage("Please provide a valid email."),
+    check("password")
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .isLength({min: 6})
+        .withMessage("Please provide a password with 6 or more characters"),
+    handleValidationErrors
+];
+
+router.post("/", validateSignup, async (req, res) => {
     const {username, email, password} = req.body;
     const newUser = await User.signUp({username, email, password});
     setTokenCookie(res, newUser);
